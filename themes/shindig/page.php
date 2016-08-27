63dbf49ca9d834f5a0d75138b527a8910ec2c977
Patrick Hoydar <sugarbuzzdesigns@gmail.com>
2016-08-26 22:04:11 -0400

    page.php update

--- themes/shindig/page.php
+++ themes/shindig/page.php
@@ -25,7 +25,7 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'content', 'page' ); ?>
 		<?php endwhile; // end of the loop. ?>
-				
+
 		<div class="clearfix"></div>
 	</div><!-- close .width-container -->
 </div><!-- close #main -->
