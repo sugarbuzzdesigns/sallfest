481d042032e1c5cd5a9bcb8ef86505c63fe66f93
Patrick Hoydar <sugarbuzzdesigns@gmail.com>
2016-08-28 22:51:52 -0400

    homepage updates

--- themes/shindig/header.php
+++ themes/shindig/header.php
@@ -36,6 +36,23 @@
 	<?php else: ?>
 	<?php endif; ?>
 	<div class="width-container">
-		<img id="home-logo" class="logo" src="http://www.sallfest.com/wp-content/uploads/2015/09/Shine_Logo.png" alt="">
+		<div class="brigade-shield">
+			<img src="<?php echo get_bloginfo('template_directory'); ?>/images/brigade-logo.png">
+		</div>
+		<img id="home-logo" class="logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/SALLFest-1450x800.png" alt="">
 	</div>
 </header>
+
+<div id="cvhn-homepage-widget" class="light-fonts-pro homepage-widget-blog widget">
+	<div class="light">
+		<div id="wrapper" class="width-container">
+			<div class="inner">
+				<h1 class="home-widget">About the Children's Volunteer Health Network</h1>
+				<img class="cvhn-logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/cvhn.png">
+
+				<p>Children’s Volunteer Health Network began in 2005, when one little boy’s crooked smile touched the heart of Tricia Carlisle-Northcutt. Tricia volunteered at an outreach program at her church, and every Tuesday, Tyler and his little brother were there “only for the cookies,” he would say. Tyler was disruptive, funny and had great leadership potential, but his jumbled mouth of teeth caused him emotional pain. He was called “Monster Mouth” and ridiculed by other children and, in turn, became a bully and show off. He was routinely suspended from school for fighting. Over a period of a few months, Tricia knew that Tyler needed to be helped or he would be a lost child. She also believed there were many more children like Tyler who could not afford dental or medical health care. Through a lot of prayer and research, the idea for Children’s Volunteer Health Network was born. Tricia Carlisle-Northcutt - Founder/President</p>
+				<p>Volunteer doctors and dentists could donate their time, and volunteers would make sure the kids got to their appointments. In theory, it sounded great. Getting there was going to be a Herculean task. Tricia called 3 people in her church community and asked them to help form this faith based organization. All three of them said, “let’s do it!” The next step was convincing an orthodontist to see Tyler at no cost. With the first phone call placed and Tyler’s story shared, Dr. Runnels said, “Yes!” Tyler would get his braces. From that day forward, there was no going back. And for Tyler it was life changing. His grades improved, he became manager of his wrestling team and he learned that there was an entire community that cared for him.</p>
+			</div>
+		</div>
+	</div>
+</div>
--- themes/shindig/widgets/map-highlight.php
+++ themes/shindig/widgets/map-highlight.php
@@ -7,22 +7,22 @@ function pyre_homepage_map_feat_load_widgets()
 }
 
 class Pyre_Latest_Map_Featured_Media_Widget extends WP_Widget {
-	
+
 	function Pyre_Latest_Map_Featured_Media_Widget()
 	{
 		$widget_ops = array('classname' => 'pyre_homepage_media-map-feat', 'description' => 'Map Highlight Widget');
 
 		$control_ops = array('id_base' => 'pyre_homepage_media-widget-map-feat');
-		
+
         add_action( 'load-widgets.php', array(&$this, 'my_custom_load') );
-		
-		
+
+
 		parent::__construct('pyre_homepage_media-widget-map-feat', 'Home: Map Widget', $widget_ops, $control_ops);
 	}
-	
-    function my_custom_load() {    
-           wp_enqueue_style( 'wp-color-picker' );        
-           wp_enqueue_script( 'wp-color-picker' );    
+
+    function my_custom_load() {
+           wp_enqueue_style( 'wp-color-picker' );
+           wp_enqueue_script( 'wp-color-picker' );
 		   if(function_exists( 'wp_enqueue_media' )){
 		       wp_enqueue_media();
 		   }else{
@@ -31,34 +31,34 @@ class Pyre_Latest_Map_Featured_Media_Widget extends WP_Widget {
 		       wp_enqueue_script('thickbox');
 		   }
        }
-	
+
 	function widget($args, $instance)
 	{
 		global $post;
-		
+
 		extract($args);
 		$title = apply_filters('widget_title', $instance['title']);
-		
+
 		$summary_text = $instance['summary_text'];
 		$map_text = $instance['map_text'];
-		
-		
+
+
 		$link_text = $instance['link_text'];
 		$link_link = $instance['link_link'];
 		$link_icon = $instance['link_icon'];
-		
+
 		$secondary_link_text = $instance['secondary_link_text'];
 		$secondary_link_link = $instance['secondary_link_link'];
 		$secondary_link_icon = $instance['secondary_link_icon'];
-		
+
 		$widget_bg = $instance['widget_bg'];
 		$widget_bg_img = $instance['widget_bg_img'];
 		$checkbox_pro = $instance['checkbox_pro'];
-		
+
 		echo $before_widget;
-		
+
 		 ?>
-		<div id="wrapper" class="width-container">
+		<div id="wrapper" class="width-container light">
 			<div class="footer-map-home <?php echo esc_attr(  $args['widget_id'] ); ?><?php if($checkbox_pro): ?> light-fonts-pro<?php endif; ?>" <?php if($widget_bg): ?>style="background-color:<?php echo esc_attr( $widget_bg ); ?>;"<?php endif; ?>>
 				<div id="map-embed-pro"><?php if($map_text): ?><?php echo do_shortcode( $map_text ); ?><?php endif; ?></div>
 				<div id="map-text-pro">
@@ -69,46 +69,46 @@ class Pyre_Latest_Map_Featured_Media_Widget extends WP_Widget {
 				<div class="clearfix"></div>
 			</div><!-- close .footer-map-home -->
 		</div>
-		
+
 		<?php if($widget_bg_img): ?>
 			<script type='text/javascript'>jQuery(document).ready(function($) {   $(".<?php echo esc_attr(  $args['widget_id'] ); ?>").backstretch([ "<?php echo esc_url( $widget_bg_img ); ?>" ],{ fade: 750, }); }); </script>
 		<?php endif; ?>
-		
+
 		<?php
 		echo $after_widget;
 	}
-	
+
 	function update($new_instance, $old_instance)
 	{
 		$instance = $old_instance;
-		
+
 		$instance['title'] = $new_instance['title'];
-		
+
 		$instance['link_text'] = $new_instance['link_text'];
 		$instance['link_link'] = $new_instance['link_link'];
 		$instance['link_icon'] = $new_instance['link_icon'];
-		
-		
-		
+
+
+
 		$instance['map_text'] = $new_instance['map_text'];
-		
+
 		$instance['summary_text'] = $new_instance['summary_text'];
-		
+
 		$instance['secondary_link_text'] = $new_instance['secondary_link_text'];
 		$instance['secondary_link_link'] = $new_instance['secondary_link_link'];
 		$instance['secondary_link_icon'] = $new_instance['secondary_link_icon'];
-		
+
 		$instance['widget_bg'] = $new_instance['widget_bg'];
 		$instance['widget_bg_img'] = $new_instance['widget_bg_img'];
-		
+
 		$instance['checkbox_pro'] = $new_instance['checkbox_pro'];
-		
+
 		return $instance;
 	}
 
 	function form($instance)
 	{
-		
+
 		$defaults = array('title' => 'LOCATION & INFORMATION', 'summary_text' => '<h4>Where Can I Park?</h4>
 When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown.
 
@@ -116,28 +116,28 @@ When she reached the first hills of the Italic Mountains, she had a last view ba
 <h4>Box Office Hours</h4>
 When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown.', 'map_text' => '[ls_googlemap location="1351 Arata Lane Windsor, CA 95492" zoom="16" title="Listing Location" height="500"]', 'link_text' => '', 'link_link' => '', 'widget_bg' => '#ffffff', 'widget_image_left_pro' => '', 'link_icon' => '', 'widget_bg_img' => '', 'checkbox_pro' => '');
 		$instance = wp_parse_args((array) $instance, $defaults); ?>
-		
+
 		<script type='text/javascript'>
 		            jQuery(document).ready(function($) {
 		                $('.my-color-picker-footer').wpColorPicker();
 		            });
 		    </script>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title', 'progression' ); ?>:</label>
 			<input class="widefat" style="width: 216px;" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $instance['title']; ?>" />
 		</p>
-		
-		
+
+
 		<p>
 			<label for="<?php echo $this->get_field_id('map_text'); ?>"><?php _e( 'Map Shortcode', 'progression' ); ?>:</label>
 			<textarea class="widefat" rows="10" cols="20" id="<?php echo $this->get_field_id('map_text'); ?>" name="<?php echo $this->get_field_name('map_text'); ?>"><?php echo $instance['map_text']; ?></textarea>
 		</p>
-	
+
 		<p>
 			<label for="<?php echo $this->get_field_id('summary_text'); ?>"><?php _e( 'Summary Text', 'progression' ); ?>:</label>
-	
-			
+
+
 			<textarea class="widefat" rows="10" cols="20" id="<?php echo $this->get_field_id('summary_text'); ?>" name="<?php echo $this->get_field_name('summary_text'); ?>"><?php echo $instance['summary_text']; ?></textarea>
 		</p>
 
@@ -145,25 +145,25 @@ When she reached the first hills of the Italic Mountains, she had a last view ba
 			<label for="<?php echo $this->get_field_id('link_text'); ?>"><?php _e( 'Button Text', 'progression' ); ?>:</label>
 			<input class="widefat" style="width: 216px;" id="<?php echo $this->get_field_id('link_text'); ?>" name="<?php echo $this->get_field_name('link_text'); ?>" value="<?php echo $instance['link_text']; ?>" />
 		</p>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id('link_link'); ?>"><?php _e( 'Button Link', 'progression' ); ?>:</label>
 			<input class="widefat" style="width: 216px;" id="<?php echo $this->get_field_id('link_link'); ?>" name="<?php echo $this->get_field_name('link_link'); ?>" value="<?php echo $instance['link_link']; ?>" />
 		</p>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id('link_icon'); ?>"><?php _e( 'Button Icon', 'progression' ); ?>:</label>
 			<input class="widefat" style="width: 216px;" id="<?php echo $this->get_field_id('link_icon'); ?>" name="<?php echo $this->get_field_name('link_icon'); ?>" value="<?php echo $instance['link_icon']; ?>" />
 		</p>
-		
-		
-		
+
+
+
 		<p>
 			<label for="<?php echo $this->get_field_id('widget_bg'); ?>"><?php _e( 'Widget Background Color', 'progression' ); ?>:</label>
 			<br>
 			<input class="my-color-picker-footer" id="<?php echo $this->get_field_id('widget_bg'); ?>" name="<?php echo $this->get_field_name('widget_bg'); ?>" value="<?php echo $instance['widget_bg']; ?>" />
 		</p>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id('widget_bg_img'); ?>"><?php _e( 'Widget Background Image', 'progression' ); ?>:</label>
 			<br>
@@ -183,21 +183,21 @@ When she reached the first hills of the Italic Mountains, she had a last view ba
 			            wp.media.editor.send.attachment = send_attachment_bkp;
 			        }
 			        wp.media.editor.open(button);
-			        return false;       
+			        return false;
 			    });
 			});
 			</script>
 			<div style=" clear:both;"></div>
 		</p>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id('checkbox_pro'); ?>"><?php _e( 'Check box for light text', 'progression' ); ?>:</label>
-			<input class="checkbox" type="checkbox" <?php checked($instance['checkbox_pro'], 'on'); ?> id="<?php echo $this->get_field_id('checkbox_pro'); ?>" name="<?php echo $this->get_field_name('checkbox_pro'); ?>" /> 
-			
+			<input class="checkbox" type="checkbox" <?php checked($instance['checkbox_pro'], 'on'); ?> id="<?php echo $this->get_field_id('checkbox_pro'); ?>" name="<?php echo $this->get_field_name('checkbox_pro'); ?>" />
+
 		</p>
-		
-	
-		
+
+
+
 	<?php }
 }
 ?>
