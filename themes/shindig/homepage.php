cce02a5264dfedb19a547742d4a50103486fd973
Patrick Hoydar <patrick.hoydar@salesloft.com>
2018-04-15 23:19:57 -0400

    signupform only if not logged in

--- themes/shindig/homepage.php
+++ themes/shindig/homepage.php
@@ -8,12 +8,13 @@
 ?>
 
 	<?php get_header(); ?>
-	 	<?php if ( !is_user_logged_in() ) : ?>
+		 <?php if ( !is_user_logged_in() ) : ?>
+		 	<?php echo do_shortcode('[ssf id=1198128905]'); ?>
+
 			<div class="width-container">
 				<img style="margin: 0 auto; display: block;" id="home-logo" class="logo" src="<?php echo get_template_directory_uri(); ?>/images/SALL2018-STD-Square-websafe.jpg" alt="">
 			</div>
 		<?php else : ?>
-
 			<div class="stretch">
 				<div class="stretch__bg background-image--home">
 				<img class="logo--home" src="<?php echo get_template_directory_uri(); ?>/images/SALLatSEA_Logo_web.png" alt="">
@@ -22,7 +23,6 @@
 			</div>
 
 			<div class="width-container">
-
 
 			<?php if( is_page_template('homepage.php') ): ?>
 				<?php if ( is_active_sidebar( 'homepage-widgets' ) ) : ?>
