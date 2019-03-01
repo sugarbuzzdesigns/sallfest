68b07071eba184beb80fed8f216b7db481e9e873
Patrick Hoydar <pshoydar@gmail.com>
2019-03-01 16:50:32 -0500

    manual autocomplete

--- themes/shindig/inc/custom/woocommerce.php
+++ themes/shindig/inc/custom/woocommerce.php
@@ -112,3 +112,16 @@ function action_woocommerce_order_item_meta_start( $item_id, $item, $order ) {
 
   echo '<div><strong>DOB</strong>: ' . $dob . '</div>';
 }
+
+/**
+ * Auto Complete all WooCommerce orders.
+ */
+add_action( 'woocommerce_thankyou', 'custom_woocommerce_auto_complete_order' );
+function custom_woocommerce_auto_complete_order( $order_id ) { 
+    if ( ! $order_id ) {
+        return;
+    }
+
+    $order = wc_get_order( $order_id );
+    $order->update_status( 'completed' );
+}
