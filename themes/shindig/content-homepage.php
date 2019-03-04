6ca840f96de7c9877f15a99a809e1b1f9143c0ca
Patrick Hoydar <pshoydar@gmail.com>
2019-03-04 08:26:44 -0500

    update hero image and video poster:

--- themes/shindig/content-homepage.php
+++ themes/shindig/content-homepage.php
@@ -6,24 +6,25 @@
  */
 ?>
 <?php get_header(); ?>
+<div class="home-bg-img">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/SALL2019_Logo.png" alt="" class="text-logo">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/SALL2019_Splash_Wide_web.jpg" alt="" class="logo-bg">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/date-text-2019.png" alt="" class="text-date">
+</div>
 <div class="width-container home-bg">
-	<div class="home-bg-img">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/SALL-2019_web.jpg" alt="">
-	</div>
-
 	<div class="video-grid grid">
 		<div class="video-column">
 			<h3>1. Booking</h3>
-			<?php echo do_shortcode('[videojs_video url="' . get_template_directory_uri() . '/video/BookSALLToday.mp4"]'); ?>
+			<?php echo do_shortcode('[videojs_video url="' . get_template_directory_uri() . '/video/BookSALLToday.mp4" poster="' . get_template_directory_uri() . '/images/video-poster.jpg"]'); ?>
 		</div>		
 		<div class="video-column">
 			<h3>2. Pre-stay</h3>
-			<?php echo do_shortcode('[videojs_video url="' . get_template_directory_uri() . '/video/PreStaySALL.mp4"]'); ?>
+			<?php echo do_shortcode('[videojs_video url="' . get_template_directory_uri() . '/video/PreStaySALL.mp4" poster="' . get_template_directory_uri() . '/images/video-poster.jpg"]'); ?>
 		</div>		
 		<div class="video-column">
 			<h3>3. Destinations</h3>
-			<?php echo do_shortcode('[videojs_video url="' . get_template_directory_uri() . '/video/DestinationsSALL.mp4"]'); ?>
-		</div>		
+			<?php echo do_shortcode('[videojs_video url="' . get_template_directory_uri() . '/video/DestinationsSALL.mp4" poster="' . get_template_directory_uri() . '/images/video-poster.jpg"]'); ?>
+		</div>
 	</div>	
 
 	<?php if( is_page_template('homepage.php') ): ?>
